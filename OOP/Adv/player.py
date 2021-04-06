8ada3e1f96ee0c32e020192bbc209d276cc7377f
Tom Tarpey <tomtarpeydev@gmail.com>
2021-04-06 17:00:49 +0100

    adventure game day 1 possible solution

---
+++ OOP/Adv/player.py
@@ -0,0 +1,16 @@
+# Write a class to hold player information, e.g. what room they are in
+# currently.
+
+class Player:
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+
+    def move(self, direction):
+        attribute = direction + '_to'
+
+        if hasattr(self.current_room, attribute):
+            self.current_room = getattr(self.current_room, attribute)
+        else:
+            print("you may not go in that direction!\n")
+
