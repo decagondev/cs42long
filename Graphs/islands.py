143c45d92966f9a07f46a148714628d3b73f7eb8
Tom Tarpey <tomtarpeydev@gmail.com>
2021-04-07 18:39:25 +0100

    island counter solution

--- Graphs/islands.py
+++ Graphs/islands.py
@@ -13,6 +13,70 @@ islands = [[0, 1, 0, 1, 0],
            [1, 1, 0, 0, 0]]
 
 
+class Stack():
+    def __init__(self):
+        self.storage = []
+    
+    def push(self, value):
+        self.storage.append(value)
+    
+    def pop(self):
+        return self.storage.pop()
+
+    def size(self):
+        return len(self.storage)
+
+
+# helper functions
+
+def  get_neighbors(row, col, islands):
+    # create an empty neighbors list
+    neighbors = []
+    # check north.
+    if row > 0 and islands[row - 1][col] == 1:
+        neighbors.append((row - 1, col))
+    # check south.
+    if row < len(islands) - 1 and islands[row + 1][col] == 1:
+        neighbors.append((row + 1, col))
+    # check east.
+    if col < len(islands[0]) - 1 and islands[row][col + 1] == 1:
+        neighbors.append((row, col + 1))
+    # check west.
+    if col > 0 and islands[row][col - 1] == 1:
+        neighbors.append((row, col - 1))
+
+    # return the neighbors to the caller
+    return neighbors
+
+def dft(row, col, islands, visited):
+    # create a intermediate data structure
+    s = Stack()
+
+    # put the starting node on to our intermediate data structure.
+    s.push( (row, col) )
+
+    # while our intermediate data structure is not empty
+    while s.size() > 0:
+        # get the node from our intermediate data structure.
+        v = s.pop()
+        # extract the row and col from our node.
+        row = v[0]
+        col = v[1]
+
+        # if our node is not in visited...
+        if not visited[row][col]:
+            # mark it as visited.
+            visited[row][col] = True
+
+            # add each of the nodes neighbors to our intermediate data structure
+            for neighbor in get_neighbors(row, col, islands):
+                s.push(neighbor)
+    
+    # return visited to the caller
+    return visited
+
+
+
 
 def island_counter(islands):
     # some visited list / dict / matrix.
@@ -24,8 +88,8 @@ def island_counter(islands):
     # set a counter to zero
     counter = 0
     # traverse the islands
-    for col in len(islands[0]):
-        for row in len(islands):
+    for col in range(len(islands[0])):
+        for row in range(len(islands)):
             # if we have not visited this island yet
             if not visited[row][col]:
                 # if it is a 1 (when we hit an actual island)
@@ -41,4 +105,17 @@ def island_counter(islands):
     # return the counter to the caller
     return counter
 
-island_counter(islands) # returns 4
+print(island_counter(islands)) # returns 4
+
+islands = [[1, 0, 0, 1, 1, 0, 1, 1, 0, 1],
+           [0, 0, 1, 1, 0, 1, 0, 0, 0, 0],
+           [0, 1, 1, 1, 0, 0, 0, 1, 0, 1],
+           [0, 0, 1, 0, 0, 1, 0, 0, 1, 1],
+           [0, 0, 1, 1, 0, 1, 0, 1, 1, 0],
+           [0, 1, 0, 1, 1, 1, 0, 1, 0, 0],
+           [0, 0, 1, 0, 0, 1, 1, 0, 0, 0],
+           [1, 0, 1, 1, 0, 0, 0, 1, 1, 0],
+           [0, 1, 1, 0, 0, 0, 1, 1, 0, 0],
+           [0, 0, 1, 1, 0, 1, 0, 0, 1, 0]]
+
+print(island_counter(islands))  # 13
